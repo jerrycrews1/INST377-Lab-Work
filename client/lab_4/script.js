57240007626ef712f43ed22450ca74e59162f4da
Jerry Crews <jerry.crews1@gmail.com>
2021-09-24 10:02:07 -0400

    Completed lab 4

---
+++ client/lab_4/script.js
@@ -0,0 +1,42 @@
+// Get the items we will be working with
+const prevButton = document.querySelector('.prev-button')
+const nextButton = document.querySelector('.next-button')
+const photos = document.querySelectorAll('.photo-grid-item')
+let currentPosition = 0 // start at the beginning
+
+const previous = async () => {
+    // go to previous image
+
+    let nextImageIndex =
+        currentPosition !== 0 ? currentPosition - 1 : photos.length - 1
+    changeImage(nextImageIndex)
+    currentPosition = nextImageIndex
+}
+
+const next = () => {
+    // go to next image
+
+    let nextImageIndex =
+        currentPosition !== photos.length - 1 ? currentPosition + 1 : 0
+    changeImage(nextImageIndex)
+    currentPosition = nextImageIndex
+}
+
+// Add event listeners to the buttons
+prevButton.addEventListener('click', previous)
+nextButton.addEventListener('click', next)
+
+const changeImage = (nextActiveIndex) => {
+    // handles changing the image
+    // change class of current item
+    console.log(nextActiveIndex)
+    console.log(photos[currentPosition])
+    console.log(photos[nextActiveIndex])
+
+    photos[currentPosition].classList.remove('active-item')
+    photos[currentPosition].classList.add('hidden-item')
+
+    photos[nextActiveIndex].classList.add('active-item')
+    photos[nextActiveIndex].classList.remove('hidden-item')
+}
+console.log(photos)
