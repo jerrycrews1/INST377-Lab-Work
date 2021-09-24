0e6ca31f5af274d415c368a307d294155ef13e4b
Jerry Crews <jerry.crews1@gmail.com>
2021-09-24 10:04:39 -0400

    cleaned up stray logs

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -4,9 +4,8 @@ const nextButton = document.querySelector('.next-button')
 const photos = document.querySelectorAll('.photo-grid-item')
 let currentPosition = 0 // start at the beginning
 
-const previous = async () => {
+const previous = () => {
     // go to previous image
-
     let nextImageIndex =
         currentPosition !== 0 ? currentPosition - 1 : photos.length - 1
     changeImage(nextImageIndex)
@@ -15,7 +14,6 @@ const previous = async () => {
 
 const next = () => {
     // go to next image
-
     let nextImageIndex =
         currentPosition !== photos.length - 1 ? currentPosition + 1 : 0
     changeImage(nextImageIndex)
@@ -29,14 +27,10 @@ nextButton.addEventListener('click', next)
 const changeImage = (nextActiveIndex) => {
     // handles changing the image
     // change class of current item
-    console.log(nextActiveIndex)
-    console.log(photos[currentPosition])
-    console.log(photos[nextActiveIndex])
-
     photos[currentPosition].classList.remove('active-item')
     photos[currentPosition].classList.add('hidden-item')
 
+    // change class of next item
     photos[nextActiveIndex].classList.add('active-item')
     photos[nextActiveIndex].classList.remove('hidden-item')
 }
-console.log(photos)
