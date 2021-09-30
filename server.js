b72d51cf2cbaea4c448225cdc1f342f49d142fb2
Jerry Crews <jerry.crews1@gmail.com>
2021-09-30 12:54:44 -0400

    Finished Lab 5

--- server.js
+++ server.js
@@ -1,59 +1,62 @@
 /* eslint-disable no-console */
-import express from 'express';
-import labRoutes from './server/labRoutes.js';
-import apiRoutes from './server/routes/apiRoutes.js';
-import reload from 'livereload';
-import connectReload from 'connect-livereload';
-import dotenv from 'dotenv';
-import path from 'path';
+import express from 'express'
+import labRoutes from './server/labRoutes.js'
+import apiRoutes from './server/routes/apiRoutes.js'
+import reload from 'livereload'
+import connectReload from 'connect-livereload'
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config();
-const __dirname = path.resolve();
+dotenv.config()
+const __dirname = path.resolve()
 // import db from './server/database/initializeDB.js';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-const staticFolder = 'client';
-let liveReloadServer;
+const app = express()
+const PORT = process.env.PORT || 3000
+const staticFolder = 'client'
+let liveReloadServer
 
 // Add some auto-reloading to our server
 if (process.env.CONTEXT === 'development') {
-  liveReloadServer = reload.createServer();
-  liveReloadServer.watch(path.join(__dirname, staticFolder));
+    liveReloadServer = reload.createServer()
+    liveReloadServer.watch(path.join(__dirname, staticFolder))
 }
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+})
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use(express.static(staticFolder));
+app.use(express.static(staticFolder))
 // app.use('/template', labRoutes);
-app.use('/api', apiRoutes);
+app.use('/api', apiRoutes)
 
 async function bootServer() {
-  try {
-    app.listen(PORT, () => {
-      // Turn these back on in later labs
-      // const mysql = await db.sequelizeDB;
-      // await mysql.sync();
-      console.log(`Listening on: http//localhost:${PORT}`);
-      console.log(`environment:`, process.env.CONTEXT);
-    });
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        app.listen(PORT, () => {
+            // Turn these back on in later labs
+            // const mysql = await db.sequelizeDB;
+            // await mysql.sync();
+            console.log(`Listening on: http://localhost:${PORT}`)
+            console.log('environment:', process.env.CONTEXT)
+        })
+    } catch (err) {
+        console.error(err)
+    }
 }
 
-bootServer();
+bootServer()
 if (process.env.CONTEXT === 'development') {
-  liveReloadServer.server.once('connection', () => {
-    setTimeout(() => {
-      liveReloadServer.refresh('/');
-    }, 100);
-  });
+    liveReloadServer.server.once('connection', () => {
+        setTimeout(() => {
+            liveReloadServer.refresh('/')
+        }, 100)
+    })
 }
