7ddb99e3839d8af6466d2634bef1be165bd40376
Jerry Crews <jerry.crews1@gmail.com>
2021-10-05 12:58:41 -0400

    Tetris - implemented rotation check function

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -237,6 +237,33 @@ document.addEventListener('DOMContentLoaded', () => {
         draw()
     }
 
+    // FIX ROTATION OF TETROMINOS A THE EDGE
+    const isAtRight = () =>
+        current.some((index) => (currentPosition + index + 1) % width === 0)
+
+    const isAtLeft = () =>
+        current.some((index) => (currentPosition + index) % width === 0)
+
+    const checkRotatedPosition = (P) => {
+        P = P || currentPosition // get current position.
+        // Then, check if the piece is near the left side.
+        if ((P + 1) % width < 4) {
+            // add 1 because the position index can be 1 less
+            // than where the piece is (with how they are indexed).
+            if (isAtRight()) {
+                // use actual position to check if it's flipped over to right side
+                currentPosition += 1 // if so, add one to wrap it back around
+                checkRotatedPosition(P) // check again.
+                // Pass position from start, since long block might need to move more.
+            }
+        } else if (P % width > 5) {
+            if (isAtLeft()) {
+                currentPosition -= 1
+                checkRotatedPosition(P)
+            }
+        }
+    }
+
     // rotate
     const rotate = () => {
         undraw()
@@ -245,6 +272,7 @@ document.addEventListener('DOMContentLoaded', () => {
             currentRotation = 0
         }
         current = theTetrominoes[random][currentRotation]
+        checkRotatedPosition()
         draw()
     }
 
@@ -260,7 +288,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 moveRight()
                 break
             case 40:
-                // moveDown()
+                moveDown()
                 break
             default:
                 break
